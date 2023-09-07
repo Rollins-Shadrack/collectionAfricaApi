e0d322ab2869d8632f17828e9a5e53cb78275774
Rollins-Shadrack <88205793+Rollins-Shadrack@users.noreply.github.com>
2023-09-07 16:03:59 +0300

    initial commit

---
+++ utils/generateToken.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+
+const generateToken = (res, userId) =>{
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+        expiresIn:'1d'
+    })
+
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'strict',
+        maxAge: 1 * 24 * 60 * 60 * 1000
+    })
+}
+
+module.exports = {
+    generateToken
+}
