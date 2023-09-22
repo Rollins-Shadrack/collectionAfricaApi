c997a7b4786c4edfeec4a2a59711914fad8a2985
Rollins-Shadrack <88205793+Rollins-Shadrack@users.noreply.github.com>
2023-09-22 18:03:26 +0300

    initial Commit

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) =>{
     res.cookie('jwt', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'none',
+        sameSite: 'strict',
         maxAge: 1 * 24 * 60 * 60 * 1000
     })
 }
