98b7318b9a6ddbac99cf35119b665b27598d1b8c
Rollins-Shadrack <88205793+Rollins-Shadrack@users.noreply.github.com>
2023-09-22 18:01:58 +0300

    initial Commit

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const multer = require('multer')
-const {Upload, uploadByLink, NewCourse, getCourses, singleCourse, UpdateCourse} = require('../controllers/admin')
+const {Upload, uploadByLink, NewCourse, getCourses, singleCourse, UpdateCourse, deleteCourse, setQuiz, getQuiz, saveCommentReply} = require('../controllers/admin')
 const {protect} = require('../middleware/authMiddleware')
 
 const router = express.Router()
@@ -16,6 +16,12 @@ router.post('/upload-video',protect,photosMiddleware.array('videos', 100),Upload
 router.route('/course').post(protect, NewCourse).get(getCourses).put(protect, UpdateCourse)
 
 router.get('/course/:id',protect ,singleCourse)
+
+router.post('/delete_course',protect ,deleteCourse)
+
+router.route('/quiz').post(protect,setQuiz).get(protect,getQuiz)
+
+router.route('/comment').post(protect, saveCommentReply)
 
 
 
