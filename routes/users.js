3478b5b335da0ba3705f6af2003649e12d4e0495
Rollins-Shadrack <88205793+Rollins-Shadrack@users.noreply.github.com>
2023-09-22 18:02:36 +0300

    initial Commit

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const {protect} = require('../middleware/authMiddleware')
 
 const router = express.Router()
 
-const {authUser, registerUser, verifyStudent, logoutUser, Upload, UpdateUserprofile, getUserprofile} = require('../controllers/users')
+const {authUser, registerUser, verifyStudent, logoutUser, Upload, UpdateUserprofile, getUserprofile, makePayment, getUsers, deleteUser, StudentPerformance} = require('../controllers/users')
 
 router.post('/auth',authUser)
 
@@ -18,6 +18,14 @@ const photosMiddleware = multer({ dest: './uploads' });
 router.post('/image', protect, photosMiddleware.array('photo', 5), Upload);
 
 router.route('/profile').get(protect,getUserprofile).put(protect,UpdateUserprofile)
+
+router.post('/payment',makePayment)
+
+router.get('/users', protect, getUsers)
+
+router.post('/delete_user', protect, deleteUser)
+
+router.route('/quiz').post(protect,StudentPerformance)
 
 
 
