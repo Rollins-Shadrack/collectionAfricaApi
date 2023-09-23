d254798b6b4fb7aa4c1958032305d672cfac295b
Rollins-Shadrack <88205793+Rollins-Shadrack@users.noreply.github.com>
2023-09-23 11:44:13 +0300

    initial commit

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(cookieParser());
 app.use(
     cors({
-        origin: 'https://collectionafrica.netlify.app', 
+        origin: 'https://collectionafrica.netlify.app',
+        methods: ['GET', 'POST'],
         credentials: true
     })
 )
